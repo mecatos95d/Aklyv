abcb67b369a2704b5fc5eb7320149dfc5b3d9a2a
Wookjae Byun <mecatos95d@kaist.ac.kr>
2017-01-16 16:50:54 +0900

    Note Function Added

--- connectsample.py
+++ connectsample.py
@@ -43,6 +43,15 @@ def parseMsg(msg):
 def getNick(msgSet):
     return msgSet[0][0].split('!')[0]
 
+def isIntStr(s): # Came From http://stackoverflow.com/questions/1265665/python-check-if-a-string-represents-an-int-without-using-try-except
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+note = []
+
 irc.connect((server, 6667)) #connects to the server
 send("USER " + botName +" " + botName +" " + botName +" :" + botDescription + "\n") #user authentication
 send("NICK " + botNick +"\n") #sets nick
@@ -66,8 +75,35 @@ while True: # In a loop
             break # Temporary Quit Code
         if text.find('Closing link') != -1:
             break
-        if len(msgSet) > 1 and msgSet[1][:2] == '*핑':
-            sendMsg(msgSet[0][2], getNick(msgSet) + ", 퐁\n")
+        
+        chat = [] # Parsed Data. Original Chat can be checked by "msgSet[1]"
+        if len(msgSet) > 1:
+            chat = msgSet[1].strip().split(' ')
+        
+        if len(chat) != 0:
+            print("PARSE| " + str(chat))
+            if chat[0] == '*핑':
+                sendMsg(msgSet[0][2], getNick(msgSet) + ", 퐁\n")
+            
+            if chat[0] == '*노트':
+                if len(chat) == 1:
+                    sendMsg(msgSet[0][2], "노트 명령입니다. *노트 <내용>으로 저장을, *노트 <번호>로 호출이 가능합니다. 봇이 종료 시 데이터는 자동적으로 삭제됩니다.")
+                else:
+                    if isIntStr(chat[1]):
+                        num = int(chat[1])
+                        if num < 0 or num >= len(note):
+                            sendMsg(msgSet[0][2], "노트 번호가 잘못되었습니다! 입력 > " + chat[1])
+                        else:
+                            print(note[num])
+                            sendMsg(msgSet[0][2], "노트 #" + chat[1] + " > " + note[num])
+                    else:
+                        rmlen = 4 # remove "*노트 "
+                        note.append(msgSet[1].strip()[rmlen:])
+                        print(str(note))
+                        sendMsg(msgSet[0][2], "노트 #" + str(len(note)-1) + "가 등록되었습니다. | " + note[-1])
+
+        
+        
         if text.find('PING') != -1: # REPLY FIRST PING
             ping_msg = text[5:]
             send("PONG " + ping_msg + "\n")
